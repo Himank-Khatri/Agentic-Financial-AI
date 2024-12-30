d0360d05227967b1ceff8d9a0fe5735495047591
Himank <k.himank.14@gmail.com>
2024-12-30 19:56:32 +0530

    fixed openai api error

--- financial_agent.py
+++ financial_agent.py
@@ -1,13 +1,19 @@
+import os
+import openai
+from dotenv import load_dotenv
 from phi.agent import Agent
 from phi.model.groq import Groq
 from phi.tools.yfinance import YFinanceTools
 from phi.tools.duckduckgo import DuckDuckGo
 
+load_dotenv()
+groq_api_key = os.getenv('GROQ_API_KEY')
+
 ## Web search agent
 web_search_agent = Agent(
     name='Web Search Agent', 
     role='Search teh web for information',
-    model=Groq(id='llama3-groq-70b-8192-tool-use-preview',),
+    model=Groq(id='llama-3.1-70b-versatile'),
     tools=[DuckDuckGo],
     instructions=['Always include sources'],
     show_tool_calls=True,
@@ -18,10 +24,21 @@ web_search_agent = Agent(
 financial_agent = Agent(
     name='Financial Agent',
     role='Process financial agent',
-    model=Groq(id='llama3-groq-70b-8192-tool-use-preview',),
+    model=Groq(id='llama-3.1-70b-versatile'),
     tools=[YFinanceTools(stock_price=True, analyst_recommendations=True, stock_fundamentals=True, company_info=True, technical_indicators=True)],
     show_tool_calls=True,
     description="You are an investment analyst that researches stock prices, analyst recommendations, and stock fundamentals.",
     instructions=["Format your response using markdown and use tables to display data where possible."],
     markdown=True
 )
+
+multi_ai_agent = Agent(
+    team=[web_search_agent, financial_agent],
+    instructions=['Always include sources', "Format your response using markdown and use tables to display data where possible."],
+    show_tool_calls=True,
+    markdown=True
+)
+
+web_search_agent.print_response("Summarize analyst recommendation and share the latest news for NVidea")
+
+
